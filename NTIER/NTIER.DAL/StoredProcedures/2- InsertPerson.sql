a078fb2af6ac12338c8ab3919b05966176f9c4d7
Mehmet Fatih AKAN <akanmf@yahoo.com>
2019-04-08 14:48:05 +0300

    prosedürler eklendi

---
+++ NTIER/NTIER.DAL/StoredProcedures/2- InsertPerson.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROC [Person].[usp_PersonInsert] 
+    @BusinessEntityID int,
+    @PersonType nchar(2),
+    @NameStyle NameStyle,
+    @Title nvarchar(8) = NULL,
+    @FirstName Name,
+    @MiddleName Name = NULL,
+    @LastName Name,
+    @Suffix nvarchar(10) = NULL,
+    @EmailPromotion int,
+    @AdditionalContactInfo xml = NULL,
+    @Demographics xml = NULL,
+    @rowguid uniqueidentifier,
+    @ModifiedDate datetime
+AS 
+BEGIN
+	INSERT INTO [Person].[Person] ([BusinessEntityID], [PersonType], [NameStyle], [Title], [FirstName], [MiddleName], [LastName], [Suffix], [EmailPromotion], [AdditionalContactInfo], [Demographics], [rowguid], [ModifiedDate])
+	SELECT @BusinessEntityID, @PersonType, @NameStyle, @Title, @FirstName, @MiddleName, @LastName, @Suffix, @EmailPromotion, @AdditionalContactInfo, @Demographics, @rowguid, @ModifiedDate
+	
+	-- Begin Return Select <- do not remove
+	SELECT [BusinessEntityID], [PersonType], [NameStyle], [Title], [FirstName], [MiddleName], [LastName], [Suffix], [EmailPromotion], [AdditionalContactInfo], [Demographics], [rowguid], [ModifiedDate]
+	FROM   [Person].[Person]
+	WHERE  [BusinessEntityID] = @BusinessEntityID
+
+END
---
+++ NTIER/NTIER.DAL/StoredProcedures/3- InsertEmployee.sql
@@ -0,0 +1,29 @@
+﻿
+CREATE PROC [HumanResources].[usp_EmployeeInsert] 
+    @BusinessEntityID int,
+    @NationalIDNumber nvarchar(15),
+    @LoginID nvarchar(256),
+    @OrganizationNode hierarchyid = NULL,
+    @JobTitle nvarchar(50),
+    @BirthDate date,
+    @MaritalStatus nchar(1),
+    @Gender nchar(1),
+    @HireDate date,
+    @SalariedFlag Flag,
+    @VacationHours smallint,
+    @SickLeaveHours smallint,
+    @CurrentFlag Flag,
+    @rowguid uniqueidentifier,
+    @ModifiedDate datetime
+AS 
+BEGIN
+	
+	INSERT INTO [HumanResources].[Employee] ([BusinessEntityID], [NationalIDNumber], [LoginID], [OrganizationNode], [JobTitle], [BirthDate], [MaritalStatus], [Gender], [HireDate], [SalariedFlag], [VacationHours], [SickLeaveHours], [CurrentFlag], [rowguid], [ModifiedDate])
+	SELECT @BusinessEntityID, @NationalIDNumber, @LoginID, @OrganizationNode, @JobTitle, @BirthDate, @MaritalStatus, @Gender, @HireDate, @SalariedFlag, @VacationHours, @SickLeaveHours, @CurrentFlag, @rowguid, @ModifiedDate
+	
+	-- Begin Return Select <- do not remove
+	SELECT [BusinessEntityID], [NationalIDNumber], [LoginID], [OrganizationNode], [OrganizationLevel], [JobTitle], [BirthDate], [MaritalStatus], [Gender], [HireDate], [SalariedFlag], [VacationHours], [SickLeaveHours], [CurrentFlag], [rowguid], [ModifiedDate]
+	FROM   [HumanResources].[Employee]
+	WHERE  [BusinessEntityID] = @BusinessEntityID
+	-- End Return Select <- do not remove
+END
