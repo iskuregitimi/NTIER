e3bf793d9f3816e146970eabf526a1f9f55c012d
hmalkan <hmalkan@gmail.com>
2019-04-08 23:25:33 +0300

    ödev yapıldı

--- NTIER/NTIER.DAL/DataContext.cs
+++ NTIER/NTIER.DAL/DataContext.cs
@@ -45,11 +45,31 @@ namespace NTIER.DAL
         public static void InsertPerson(Person person)
         {
             //TODO: insert person kodu yazılacak
+            SqlCommand cmd = new SqlCommand("INSERT_PERSON", Connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@BusinessEntityID", SqlDbType.VarChar).Value = person.BusinessEntityID;
+            cmd.Parameters.Add("@Title", SqlDbType.VarChar).Value = person.Title;
+            cmd.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = person.FirstName;
+            cmd.Parameters.Add("@MiddleName", SqlDbType.VarChar).Value = person.MiddleName;
+            cmd.Parameters.Add("@LastName", SqlDbType.VarChar).Value = person.LastName;
+
+            Connection.Open();
+            cmd.ExecuteNonQuery();
+            Connection.Close();
         }
 
         public static void InsertEmployee(Employee employee)
         {
             //TODO: insert employee kodu yazılacak
+            SqlCommand cmd = new SqlCommand("INSERT_EMPLOYEE", Connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@BusinessEntityID", SqlDbType.VarChar).Value = employee.BusinessEntityID;
+            cmd.Parameters.Add("@JobTitle", SqlDbType.VarChar).Value = employee.JobTitle;
+            cmd.Parameters.Add("@BirthDate", SqlDbType.VarChar).Value = employee.BirthDate;
+
+            Connection.Open();
+            cmd.ExecuteNonQuery();
+            Connection.Close();
         }
     }
 }
