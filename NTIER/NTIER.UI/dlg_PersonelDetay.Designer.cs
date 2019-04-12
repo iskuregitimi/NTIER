2d82dd9c39911a976ccb6036a029136dabd0fa4a
Mehmet Fatih AKAN <akanmf@yahoo.com>
2019-04-12 09:30:01 +0300

    Email silme eklendi

--- NTIER/NTIER.UI/dlg_PersonelDetay.Designer.cs
+++ NTIER/NTIER.UI/dlg_PersonelDetay.Designer.cs
@@ -28,171 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.components = new System.ComponentModel.Container();
-			this.dgv_Adres = new System.Windows.Forms.DataGridView();
-			this.dgv_Tel = new System.Windows.Forms.DataGridView();
-			this.dgv_Email = new System.Windows.Forms.DataGridView();
-			this.lbl_Id = new System.Windows.Forms.Label();
-			this.lbl_JobTitle = new System.Windows.Forms.Label();
-			this.lbl_HireDate = new System.Windows.Forms.Label();
-			this.lbl_Name = new System.Windows.Forms.Label();
-			this.lbl_Surname = new System.Windows.Forms.Label();
-			this.dgv_PersonelBilgileri = new System.Windows.Forms.DataGridView();
-			this.lbl_BirthDate = new System.Windows.Forms.Label();
-			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-			this.silToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.güncelleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			((System.ComponentModel.ISupportInitialize)(this.dgv_Adres)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.dgv_Tel)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.dgv_Email)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.dgv_PersonelBilgileri)).BeginInit();
-			this.contextMenuStrip1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// dgv_Adres
-			// 
-			this.dgv_Adres.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dgv_Adres.ContextMenuStrip = this.contextMenuStrip1;
-			this.dgv_Adres.Location = new System.Drawing.Point(58, 211);
-			this.dgv_Adres.Name = "dgv_Adres";
-			this.dgv_Adres.Size = new System.Drawing.Size(589, 96);
-			this.dgv_Adres.TabIndex = 1;
-			// 
-			// dgv_Tel
-			// 
-			this.dgv_Tel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dgv_Tel.ContextMenuStrip = this.contextMenuStrip1;
-			this.dgv_Tel.Location = new System.Drawing.Point(58, 332);
-			this.dgv_Tel.Name = "dgv_Tel";
-			this.dgv_Tel.Size = new System.Drawing.Size(589, 96);
-			this.dgv_Tel.TabIndex = 3;
-			// 
-			// dgv_Email
-			// 
-			this.dgv_Email.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dgv_Email.ContextMenuStrip = this.contextMenuStrip1;
-			this.dgv_Email.Location = new System.Drawing.Point(58, 447);
-			this.dgv_Email.Name = "dgv_Email";
-			this.dgv_Email.Size = new System.Drawing.Size(589, 92);
-			this.dgv_Email.TabIndex = 2;
-			this.dgv_Email.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_Email_CellContentClick);
-			// 
-			// lbl_Id
-			// 
-			this.lbl_Id.AutoSize = true;
-			this.lbl_Id.Location = new System.Drawing.Point(87, 45);
-			this.lbl_Id.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-			this.lbl_Id.Name = "lbl_Id";
-			this.lbl_Id.Size = new System.Drawing.Size(35, 13);
-			this.lbl_Id.TabIndex = 4;
-			this.lbl_Id.Text = "label1";
-			// 
-			// lbl_JobTitle
-			// 
-			this.lbl_JobTitle.AutoSize = true;
-			this.lbl_JobTitle.Location = new System.Drawing.Point(180, 45);
-			this.lbl_JobTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-			this.lbl_JobTitle.Name = "lbl_JobTitle";
-			this.lbl_JobTitle.Size = new System.Drawing.Size(35, 13);
-			this.lbl_JobTitle.TabIndex = 5;
-			this.lbl_JobTitle.Text = "label2";
-			// 
-			// lbl_HireDate
-			// 
-			this.lbl_HireDate.AutoSize = true;
-			this.lbl_HireDate.Location = new System.Drawing.Point(276, 45);
-			this.lbl_HireDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-			this.lbl_HireDate.Name = "lbl_HireDate";
-			this.lbl_HireDate.Size = new System.Drawing.Size(35, 13);
-			this.lbl_HireDate.TabIndex = 6;
-			this.lbl_HireDate.Text = "label3";
-			// 
-			// lbl_Name
-			// 
-			this.lbl_Name.AutoSize = true;
-			this.lbl_Name.Location = new System.Drawing.Point(362, 45);
-			this.lbl_Name.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-			this.lbl_Name.Name = "lbl_Name";
-			this.lbl_Name.Size = new System.Drawing.Size(35, 13);
-			this.lbl_Name.TabIndex = 7;
-			this.lbl_Name.Text = "label4";
-			// 
-			// lbl_Surname
-			// 
-			this.lbl_Surname.AutoSize = true;
-			this.lbl_Surname.Location = new System.Drawing.Point(452, 45);
-			this.lbl_Surname.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-			this.lbl_Surname.Name = "lbl_Surname";
-			this.lbl_Surname.Size = new System.Drawing.Size(35, 13);
-			this.lbl_Surname.TabIndex = 8;
-			this.lbl_Surname.Text = "label5";
-			// 
-			// dgv_PersonelBilgileri
-			// 
-			this.dgv_PersonelBilgileri.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dgv_PersonelBilgileri.ContextMenuStrip = this.contextMenuStrip1;
-			this.dgv_PersonelBilgileri.Location = new System.Drawing.Point(58, 91);
-			this.dgv_PersonelBilgileri.Name = "dgv_PersonelBilgileri";
-			this.dgv_PersonelBilgileri.Size = new System.Drawing.Size(589, 96);
-			this.dgv_PersonelBilgileri.TabIndex = 10;
-			// 
-			// lbl_BirthDate
-			// 
-			this.lbl_BirthDate.AutoSize = true;
-			this.lbl_BirthDate.Location = new System.Drawing.Point(561, 45);
-			this.lbl_BirthDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-			this.lbl_BirthDate.Name = "lbl_BirthDate";
-			this.lbl_BirthDate.Size = new System.Drawing.Size(35, 13);
-			this.lbl_BirthDate.TabIndex = 9;
-			this.lbl_BirthDate.Text = "label6";
-			// 
-			// contextMenuStrip1
-			// 
-			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.components = new System.ComponentModel.Container();
+            this.dgv_Adres = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.silToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.güncelleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgv_Tel = new System.Windows.Forms.DataGridView();
+            this.dgv_Email = new System.Windows.Forms.DataGridView();
+            this.lbl_Id = new System.Windows.Forms.Label();
+            this.lbl_JobTitle = new System.Windows.Forms.Label();
+            this.lbl_HireDate = new System.Windows.Forms.Label();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.lbl_Surname = new System.Windows.Forms.Label();
+            this.dgv_PersonelBilgileri = new System.Windows.Forms.DataGridView();
+            this.lbl_BirthDate = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Adres)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Tel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Email)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_PersonelBilgileri)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_Adres
+            // 
+            this.dgv_Adres.AllowUserToAddRows = false;
+            this.dgv_Adres.AllowUserToDeleteRows = false;
+            this.dgv_Adres.AllowUserToOrderColumns = true;
+            this.dgv_Adres.AllowUserToResizeRows = false;
+            this.dgv_Adres.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_Adres.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Adres.Location = new System.Drawing.Point(77, 260);
+            this.dgv_Adres.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgv_Adres.Name = "dgv_Adres";
+            this.dgv_Adres.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_Adres.Size = new System.Drawing.Size(785, 118);
+            this.dgv_Adres.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.silToolStripMenuItem,
             this.güncelleToolStripMenuItem});
-			this.contextMenuStrip1.Name = "contextMenuStrip1";
-			this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
-			this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
-			// 
-			// silToolStripMenuItem
-			// 
-			this.silToolStripMenuItem.Name = "silToolStripMenuItem";
-			this.silToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-			this.silToolStripMenuItem.Text = "Sil";
-			this.silToolStripMenuItem.Click += new System.EventHandler(this.silToolStripMenuItem_Click);
-			// 
-			// güncelleToolStripMenuItem
-			// 
-			this.güncelleToolStripMenuItem.Name = "güncelleToolStripMenuItem";
-			this.güncelleToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
-			this.güncelleToolStripMenuItem.Text = "Güncelle";
-			// 
-			// dlg_PersonelDetay
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(717, 568);
-			this.Controls.Add(this.dgv_PersonelBilgileri);
-			this.Controls.Add(this.lbl_BirthDate);
-			this.Controls.Add(this.lbl_Surname);
-			this.Controls.Add(this.lbl_Name);
-			this.Controls.Add(this.lbl_HireDate);
-			this.Controls.Add(this.lbl_JobTitle);
-			this.Controls.Add(this.lbl_Id);
-			this.Controls.Add(this.dgv_Tel);
-			this.Controls.Add(this.dgv_Email);
-			this.Controls.Add(this.dgv_Adres);
-			this.Name = "dlg_PersonelDetay";
-			this.Text = "Detaylar";
-			this.Load += new System.EventHandler(this.dlg_PersonelDetay_Load);
-			((System.ComponentModel.ISupportInitialize)(this.dgv_Adres)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.dgv_Tel)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.dgv_Email)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.dgv_PersonelBilgileri)).EndInit();
-			this.contextMenuStrip1.ResumeLayout(false);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(136, 52);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // silToolStripMenuItem
+            // 
+            this.silToolStripMenuItem.Name = "silToolStripMenuItem";
+            this.silToolStripMenuItem.Size = new System.Drawing.Size(135, 24);
+            this.silToolStripMenuItem.Text = "Sil";
+            this.silToolStripMenuItem.Click += new System.EventHandler(this.silToolStripMenuItem_Click);
+            // 
+            // güncelleToolStripMenuItem
+            // 
+            this.güncelleToolStripMenuItem.Name = "güncelleToolStripMenuItem";
+            this.güncelleToolStripMenuItem.Size = new System.Drawing.Size(135, 24);
+            this.güncelleToolStripMenuItem.Text = "Güncelle";
+            // 
+            // dgv_Tel
+            // 
+            this.dgv_Tel.AllowUserToAddRows = false;
+            this.dgv_Tel.AllowUserToDeleteRows = false;
+            this.dgv_Tel.AllowUserToOrderColumns = true;
+            this.dgv_Tel.AllowUserToResizeRows = false;
+            this.dgv_Tel.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_Tel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Tel.Location = new System.Drawing.Point(77, 409);
+            this.dgv_Tel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgv_Tel.Name = "dgv_Tel";
+            this.dgv_Tel.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_Tel.Size = new System.Drawing.Size(785, 118);
+            this.dgv_Tel.TabIndex = 3;
+            // 
+            // dgv_Email
+            // 
+            this.dgv_Email.AllowUserToAddRows = false;
+            this.dgv_Email.AllowUserToDeleteRows = false;
+            this.dgv_Email.AllowUserToOrderColumns = true;
+            this.dgv_Email.AllowUserToResizeRows = false;
+            this.dgv_Email.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_Email.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Email.ContextMenuStrip = this.contextMenuStrip1;
+            this.dgv_Email.Location = new System.Drawing.Point(77, 550);
+            this.dgv_Email.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgv_Email.Name = "dgv_Email";
+            this.dgv_Email.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_Email.Size = new System.Drawing.Size(785, 113);
+            this.dgv_Email.TabIndex = 2;
+            this.dgv_Email.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_Email_CellContentClick);
+            // 
+            // lbl_Id
+            // 
+            this.lbl_Id.AutoSize = true;
+            this.lbl_Id.Location = new System.Drawing.Point(116, 55);
+            this.lbl_Id.Name = "lbl_Id";
+            this.lbl_Id.Size = new System.Drawing.Size(46, 17);
+            this.lbl_Id.TabIndex = 4;
+            this.lbl_Id.Text = "label1";
+            // 
+            // lbl_JobTitle
+            // 
+            this.lbl_JobTitle.AutoSize = true;
+            this.lbl_JobTitle.Location = new System.Drawing.Point(240, 55);
+            this.lbl_JobTitle.Name = "lbl_JobTitle";
+            this.lbl_JobTitle.Size = new System.Drawing.Size(46, 17);
+            this.lbl_JobTitle.TabIndex = 5;
+            this.lbl_JobTitle.Text = "label2";
+            // 
+            // lbl_HireDate
+            // 
+            this.lbl_HireDate.AutoSize = true;
+            this.lbl_HireDate.Location = new System.Drawing.Point(368, 55);
+            this.lbl_HireDate.Name = "lbl_HireDate";
+            this.lbl_HireDate.Size = new System.Drawing.Size(46, 17);
+            this.lbl_HireDate.TabIndex = 6;
+            this.lbl_HireDate.Text = "label3";
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Location = new System.Drawing.Point(483, 55);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(46, 17);
+            this.lbl_Name.TabIndex = 7;
+            this.lbl_Name.Text = "label4";
+            // 
+            // lbl_Surname
+            // 
+            this.lbl_Surname.AutoSize = true;
+            this.lbl_Surname.Location = new System.Drawing.Point(603, 55);
+            this.lbl_Surname.Name = "lbl_Surname";
+            this.lbl_Surname.Size = new System.Drawing.Size(46, 17);
+            this.lbl_Surname.TabIndex = 8;
+            this.lbl_Surname.Text = "label5";
+            // 
+            // dgv_PersonelBilgileri
+            // 
+            this.dgv_PersonelBilgileri.AllowUserToAddRows = false;
+            this.dgv_PersonelBilgileri.AllowUserToDeleteRows = false;
+            this.dgv_PersonelBilgileri.AllowUserToOrderColumns = true;
+            this.dgv_PersonelBilgileri.AllowUserToResizeRows = false;
+            this.dgv_PersonelBilgileri.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_PersonelBilgileri.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_PersonelBilgileri.Location = new System.Drawing.Point(77, 112);
+            this.dgv_PersonelBilgileri.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgv_PersonelBilgileri.Name = "dgv_PersonelBilgileri";
+            this.dgv_PersonelBilgileri.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_PersonelBilgileri.Size = new System.Drawing.Size(785, 118);
+            this.dgv_PersonelBilgileri.TabIndex = 10;
+            // 
+            // lbl_BirthDate
+            // 
+            this.lbl_BirthDate.AutoSize = true;
+            this.lbl_BirthDate.Location = new System.Drawing.Point(748, 55);
+            this.lbl_BirthDate.Name = "lbl_BirthDate";
+            this.lbl_BirthDate.Size = new System.Drawing.Size(46, 17);
+            this.lbl_BirthDate.TabIndex = 9;
+            this.lbl_BirthDate.Text = "label6";
+            // 
+            // dlg_PersonelDetay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(956, 699);
+            this.Controls.Add(this.dgv_PersonelBilgileri);
+            this.Controls.Add(this.lbl_BirthDate);
+            this.Controls.Add(this.lbl_Surname);
+            this.Controls.Add(this.lbl_Name);
+            this.Controls.Add(this.lbl_HireDate);
+            this.Controls.Add(this.lbl_JobTitle);
+            this.Controls.Add(this.lbl_Id);
+            this.Controls.Add(this.dgv_Tel);
+            this.Controls.Add(this.dgv_Email);
+            this.Controls.Add(this.dgv_Adres);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "dlg_PersonelDetay";
+            this.Text = "Detaylar";
+            this.Load += new System.EventHandler(this.dlg_PersonelDetay_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Adres)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Tel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Email)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_PersonelBilgileri)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
